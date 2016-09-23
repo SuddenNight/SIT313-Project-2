80e3f1f03bfe14a09e6bedc1e28b093ee15ccfaf
SuddenNight <edsmith0012@gmail.com>
2016-09-23 12:01:56 +1000

    Add files via upload

---
+++ Project2/Project2Page.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Project2
+{
+	public partial class Project2Page : ContentPage
+	{
+		public Project2Page()
+		{
+			InitializeComponent();
+		}
+		async void OnClickStart(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new ShoppingList());
+		}
+	}
+}
---
+++ Project2/ShoppingList.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Project2
+{
+	public partial class ShoppingList : ContentPage
+	{
+		public ShoppingList()
+		{
+			InitializeComponent();
+
+		}
+
+	}
+
+}
+
